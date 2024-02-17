c4e02dee413f71974e62cde38f1c0add6fe90266
LDH <daniel_navarrete_05@hotmail.com>
2024-02-16 18:49:41 -0600

    Game Assets

---
+++ SimplePvZ/Assets/LeinadDH/Sprites/Code/SlotMachine/SlotMachine.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.LeinadDH.ChessDefense
+{
+    public class SlotMachine : MonoBehaviour
+    {
+        void Start()
+        {
+            
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
